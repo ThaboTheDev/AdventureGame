3fb5c90f61cbd1a7bf2534b02fa5994dae3e181e
thmlilojhb024 <thmlilojhb024@student.wethinkcode.co.za>
2025-07-17 15:00:38 +0200

    refactor(move_command.dart): chnaged the movement login for cardinal position based to coordinate based

--- lib/services/commands/move_command.dart
+++ lib/services/commands/move_command.dart
@@ -1,16 +1,28 @@
 import 'package:adventure_game_version_1/services/classes/player.dart';
 import 'package:adventure_game_version_1/services/commands/command.dart';
+import 'package:adventure_game_version_1/services/print_color_code.dart';
 
 class MoveCommand extends Command {
-  //ignore: unused_field
-  final String _name;
-  final String _argument;
 
   @override
   bool execute(Player target) {
-    target.move(_argument);
-    return true;
+    if (target.upadtePosition(name, int.parse(argument))) {
+      print(
+        PrintColorCode().colorize(
+          "You moved $argument steps $name",
+          PrintColorCode.green,
+        ),
+      );
+      return true;
+    }
+    print(
+      PrintColorCode().colorize(
+        "You failed to move $argument steps $name",
+        PrintColorCode.red,
+      ),
+    );
+    return false;
   }
 
-  MoveCommand(this._name, this._argument) : super(_name, _argument);
+  MoveCommand(super._name, super._argument);
 }
