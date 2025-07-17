a7f2fe59029ec1bdb83d474e62d71afafbf69ba3
thmlilojhb024 <thmlilojhb024@student.wethinkcode.co.za>
2025-07-17 12:48:10 +0200

    feat(Position.dart): added the isinside function to check if the generated position is in the room

--- lib/services/position.dart
+++ lib/services/position.dart
@@ -7,10 +7,16 @@ class Position {
   int get getX => _x;
   int get getY => _y;
 
+  bool isInside(Position topLeft, Position bottomRight) {
+    bool isInX = _x >= topLeft.getX && _x <= bottomRight.getX;
+    bool isInY = _y <= topLeft.getY && _y >= bottomRight.getY;
+    return isInX && isInY;
+  }
+
   @override
   bool operator ==(Object other) {
     if (runtimeType != other.runtimeType) return false;
-    if (identical(other,this)) return true;
+    if (identical(other, this)) return true;
     Position otherPosition = other as Position;
     return getX == otherPosition.getX && getY == otherPosition.getY;
   }
